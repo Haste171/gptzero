b88fd106880736f17ef79205d23294ec82c10d30
Crspy2 <crspy8687@gmail.com>
2023-04-02 13:01:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gptzero',
+    name='gptzero-async',
     version='0.1.1',
-    description='Python wrapper for the GPTZero API',
+    description='Asynchronous Python wrapper for the GPTZero API',
     author='David Peterson',
     author_email='dapanon@proton.me',
     url='https://github.com/haste171/gptzero',
     packages=find_packages(),
-    install_requires=['requests'],
+    install_requires=['aiohttp'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -19,6 +19,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
